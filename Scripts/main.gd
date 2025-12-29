7ea4e7f7f9c9dc10d669ff041fc4556d4a7d20e7
Sreenair-1 <sreeharinair514@gmail.com>
2025-12-29 17:43:03 +0530

    Paddle, ball

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,44 +1,48 @@
 extends Node2D
 
-var ball_speed = 300
-var paddle_speed = 300
-var ball_direction = Vector2(-1, 0)
+var ball_direction = Vector2(0, 1)
+@onready var o_paddle = $OpponentPaddle
+@onready var p_paddle = $PlayerPaddle
+@onready var ball = $Ball
+var ball_speed
+var paddle_speed
 
 func _ready():
-	pass
+	ball_speed = ball.ball_speed
+	paddle_speed = p_paddle.paddle_speed
 
 func _process(delta):
 	# Ball movement
-	$Ball.position += ball_direction * ball_speed * delta
+	ball.position += ball_direction * ball_speed * delta
 
 	# Player paddle movement
-	if Input.is_action_pressed("ui_up"):
-		$PlayerPaddle.position.y -= paddle_speed * delta
-	if Input.is_action_pressed("ui_down"):
-		$PlayerPaddle.position.y += paddle_speed * delta
+	if Input.is_action_pressed("w"):
+		p_paddle.position.y -= paddle_speed * delta
+	if Input.is_action_pressed("s"):
+		p_paddle.position.y += paddle_speed * delta
 
 	# Opponent paddle movement (simple AI)
-	if $Ball.position.y > $OpponentPaddle.position.y:
-		$OpponentPaddle.position.y += paddle_speed * delta
-	if $Ball.position.y < $OpponentPaddle.position.y:
-		$OpponentPaddle.position.y -= paddle_speed * delta
+	if ball.position.y > o_paddle.position.y:
+		o_paddle.position.y += paddle_speed * delta
+	if ball.position.y < o_paddle.position.y:
+		o_paddle.position.y -= paddle_speed * delta
 
 	# Wall collisions
-	if $Ball.position.y < 0 or $Ball.position.y > get_viewport().size.y:
+	if ball.position.y < 0 or ball.position.y > get_viewport().size.y:
 		ball_direction.y = -ball_direction.y
 
 	# Paddle collisions
-	if $Ball.get_node("Area2D").overlaps_body($PlayerPaddle) or $Ball.get_node("Area2D").overlaps_body($OpponentPaddle):
+	if ball.get_node("Area2D").overlaps_body(p_paddle.get_node("Area2D")) or ball.get_node("Area2D").overlaps_body(o_paddle.get_node("Area2D")):
 		ball_direction.x = -ball_direction.x
 
 	# Score
-	if $Ball.position.x < 0:
+	if ball.position.x < 0:
 		$OpponentScore.text = str(int($OpponentScore.text) + 1)
 		reset_ball()
-	if $Ball.position.x > get_viewport().size.x:
+	if ball.position.x > get_viewport().size.x:
 		$PlayerScore.text = str(int($PlayerScore.text) + 1)
 		reset_ball()
 
 func reset_ball():
-	$Ball.position = get_viewport().size / 2
+	ball.position = get_viewport().size / 2
 	ball_direction = Vector2(-1, 0)
