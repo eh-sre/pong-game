bf1a2a329d5da451b537474fc1503abec3ae1c86
eh-sre <ss.nair514@gmail.com>
2026-09-04 01:20:07 +0530

    ...

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var ball_speed = 300
-signal hit
+var bounce = 0
 signal out_of_bounds
 
 func _ready():
@@ -11,15 +11,23 @@ func _ready():
 func _physics_process(delta):
 	var collision = move_and_collide(velocity*delta)
 	if collision:
+		bounce+=1
 		var collider = collision.get_collider()
 		# Paddle collisions
 		if collider.is_in_group("paddles"):
-			hit.emit()
+			bounce = 1
+			velocity+=Vector2(10,10)
 		velocity = velocity.bounce(collision.get_normal())
+		if bounce > 1:
+			bounce =-1
+			velocity+=Vector2(10,-30)
+		elif bounce == 0:
+			velocity-=Vector2(10,-30)
 	
 	# Score
 	if position.x < 0 or position.x > get_viewport().size.x:
 		out_of_bounds.emit()
+	
 
 func reset():
 	velocity = Vector2(ball_speed, 150)
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,16 +4,6 @@ extends Node2D
 @onready var player_score = $PlayerScore
 @onready var opponent_score = $OpponentScore
 
-
-func _ready():
-	ball.hit.connect(_on_ball_hit)
-	ball.out_of_bounds.connect(_on_ball_out_of_bounds)
-
-
-func _on_ball_hit():
-	pass
-
-
 func _on_ball_out_of_bounds():
 	if ball.position.x < 0:
 		opponent_score.add_point()
