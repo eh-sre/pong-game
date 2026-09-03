46a5943aca1ed2f9b0477a4827e0dddac14dc76e
Sreenair-1 <sreeharinair514@gmail.com>
2026-09-03 18:26:52 +0530

    Refactor ball, paddle, and main scenes; update collision shapes and resource paths

--- Scripts/paddle.gd
+++ Scripts/paddle.gd
@@ -1,15 +1,3 @@
-extends Node2D
+extends StaticBody2D
 
 @export var paddle_speed = 300
-
-func move(delta, direction):
-	position.y += paddle_speed*delta*direction
-	print(position.y)
-
-func move_random(delta):
-	var direction = randi_range(-1, 1)
-	var modifier = randi_range(1, 6)
-	position.y += paddle_speed*delta*direction*modifier
-
-func move_to_centre(delta):
-	position.y = (324)
