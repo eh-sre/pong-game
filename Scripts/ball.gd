c66892331943aaea512387c979c11ba13664ecaa
eh-sre <ss.nair514@gmail.com>
2026-09-06 18:09:54 +0530

    Updated the ball movement and trajectory calculation

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,34 +1,52 @@
 extends CharacterBody2D
 
 @export var ball_speed = 300
-var bounce = 0
 signal out_of_bounds
 
 func _ready():
-	velocity = Vector2(ball_speed, 150)
+	var direction = [-1, 1].pick_random()
+	var vertical_direction = randf_range(-1, 1)
+	velocity = Vector2(direction, vertical_direction).normalized()*ball_speed
 
 # Ball movement
 func _physics_process(delta):
-	var collision = move_and_collide(velocity*delta)
+	var collision = move_and_collide(velocity * delta)
+
 	if collision:
-		bounce+=1
+		print(velocity.y)
 		var collider = collision.get_collider()
+
+		# Bounce
+		velocity = velocity.bounce(collision.get_normal())
+
 		# Paddle collisions
 		if collider.is_in_group("paddles"):
-			bounce = 1
-			velocity+=Vector2(10,10)
-		velocity = velocity.bounce(collision.get_normal())
-		if bounce > 1:
-			bounce =-1
-			velocity+=Vector2(10,-30)
-		elif bounce == 0:
-			velocity-=Vector2(10,-30)
-	
+			velocity *= 1.05
+			var hit_offset = position.y - collider.position.y
+			var angle = deg_to_rad(clamp(hit_offset / 10, -45, 45))
+			velocity = velocity.rotated(angle)
+
+		# Wall collisions
+		if collider.is_in_group("walls"):
+			velocity *= 1.005
+			var angle = velocity.angle()
+			var horizontal_direction = sign(velocity.x)
+			if horizontal_direction > 0:
+				angle = clamp(angle, deg_to_rad(-60), deg_to_rad(60))
+			else:
+				angle = clamp(angle, deg_to_rad(120), deg_to_rad(240))
+			velocity = Vector2.from_angle(angle) * velocity.length()
+
 	# Score
 	if position.x < 0 or position.x > get_viewport().size.x:
 		out_of_bounds.emit()
+
+func rotate_ball(angle):
+	velocity = velocity.rotated(deg_to_rad(angle))
 	
 
 func reset():
-	velocity = Vector2(ball_speed, 150)
-	position = get_viewport().size/2
+	var direction = [-1, 1].pick_random()
+	var vertical_direction = randf_range(-1, 1)
+	velocity = Vector2(direction, vertical_direction).normalized() * ball_speed
+	position = get_viewport().size / 2
